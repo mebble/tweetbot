9033b7b4cbc8819f707379a6e3a0bef5b8f1f577
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-10-13 14:45:37 +0530

    reply to all tweets mentioning bot

--- bot.js
+++ bot.js
@@ -5,15 +5,46 @@ const config = require('./config');
 
 let T = new Twit(config);
 
-/* STREAM request (user stream) - tweet */
+/* STREAM request (user stream) - tweet & reply */
 let stream = T.stream('user');
 stream.on('tweet', tweetEvent);
 
 function tweetEvent(event) {
-	const fs = require('fs');
-	let jsonStr = JSON.stringify(event, null, 4);
-	fs.writeFile(`tweets/tweet-${event.id_str.slice(0, 5)}.json`, jsonStr);
+	let replyTo = event.in_reply_to_screen_name;
+	let from = event.user.screen_name;
+	let text = event.text;
+
+	console.log(`From @${from} to @${replyTo}`);
+
+	if (replyTo === 'nottiebottie') {
+		let newTweet = `@${from} Thank you for acknowledging my existence!`;
+		tweetIt({
+			status: newTweet
+		});
+	}
 }
+
+function tweetIt(tweet) {
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log('Something went wrong');
+		} else {
+			console.log('It worked!');
+		}
+	}
+}
+
+/* STREAM request (user stream) - tweet & write to file */
+// let stream = T.stream('user');
+// stream.on('tweet', tweetEvent);
+
+// function tweetEvent(event) {
+// 	const fs = require('fs');
+// 	let jsonStr = JSON.stringify(event, null, 4);
+// 	fs.writeFile(`tweets/tweet-${event.id_str.slice(0, 5)}.json`, jsonStr);
+// }
 
 /* STREAM request (user stream) - follow */
 // let stream = T.stream('user');
